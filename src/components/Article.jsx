2065134f4f73bee9d558bea3079da8c17556cfdc
gunayhamzayeva <136006167+gunayhamzayava@users.noreply.github.com>
2023-07-11 00:17:19 +0400

    changed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -31,10 +31,39 @@ function Article() {
         <p>{data.trends}</p>
         <h5>{data.comments}</h5>
         <ul>
-          <li>We were able to run the cluster on smaller machines resulting in <b>~75%</b> reduction in individual instances’ <b>CPU</b> and <b>Memory</b> requirements.</li>
-          <li>We observed significant improvement in average and tail latencies.</li>
-          <li>Smaller index per shard also improved <b>scale-up</b> time by <b>~20%</b>.</li>
+          <li>
+            We were able to run the cluster on smaller machines resulting in{" "}
+            <b>~75%</b> reduction in individual instances’ <b>CPU</b> and{" "}
+            <b>Memory</b> requirements.
+          </li>
+          <li>
+            We observed significant improvement in average and tail latencies.
+          </li>
+          <li>
+            Smaller index per shard also improved <b>scale-up</b> time by{" "}
+            <b>~20%</b>.
+          </li>
         </ul>
+        <div className="social__media">
+          <a href="https://www.facebook.com" target="_blank">
+            <div className="face">
+              <img src="photos/face.avif" />
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fblog.zomato.com%2Ffrom-the-streets-of-bharat"
+            target="_blank"
+          >
+            <div className="linkedin">
+              <img src="photos/linkedin.avif" />
+            </div>
+          </a>
+          <a href="https://www.twitter.com" target="_blank">
+            <div className="twitter">
+              <img src="photos/tvit.avif" />
+            </div>
+          </a>
+        </div>
       </div>
     </>
   );
